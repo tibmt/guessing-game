da8595457f6d0b14b6d8f6aafb50ff7215bba5a5
Pavel <pavelyukhnevich@yandex.ru>
2017-01-28 20:46:13 +0300

    'Guessing-game'

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+       
+       let result;
+       result = Math.round((this.min + this.max) / 2);
 
+       this.solutionCandidate = result;
+       return result;
     }
-
+    
     lower() {
-
+        this.max = this.solutionCandidate;
     }
 
     greater() {
-
+        this.min = this.solutionCandidate;
     }
 }
 
